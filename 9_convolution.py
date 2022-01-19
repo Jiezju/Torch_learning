ee55d54a7cb59e9fa5a5c7d977c04874ff70c3b1
Jiezju <21725100@zju.edu.cn>
2022-01-19 23:21:36 +0800

    [convolution] update code

--- 9_convolution.py
+++ 9_convolution.py
@@ -18,11 +18,12 @@ print(out.shape)    # torch.Size([1, 4, 2, 2])
 # conv
 
 def convolution(x, bias, kernel, stride=1, padding=0):
-    in_h, in_w = x.shape
-    k_h, k_w = kernel.shape
 
     if padding > 0:
         F.pad(x, (padding, padding, padding, padding))
+
+    in_h, in_w = x.shape
+    k_h, k_w = kernel.shape
 
     out_h = (in_h - k_h) // stride + 1
     out_w = (in_w - k_w) // stride + 1
@@ -35,9 +36,64 @@ def convolution(x, bias, kernel, stride=1, padding=0):
 
     return output
 
+# 基于矩阵乘法实现卷积
+def convolution_matrix(x, bias, kernel, stride=1, padding=0):
+    if padding > 0:
+        F.pad(x, (padding, padding, padding, padding))
+
+    in_h, in_w = x.shape
+    k_h, k_w = kernel.shape
+
+    out_h = (in_h - k_h) // stride + 1
+    out_w = (in_w - k_w) // stride + 1
+
+    region_matrix = t.zeros(out_h*out_w, k_h*k_w).float()
+    kernel_matrix = kernel.reshape(kernel.numel(), 1)
+    row_index = 0
+    for i in range(0, in_h - k_h + 1, stride):
+        for j in range(0, in_w - k_w + 1, stride):
+            region = x[i:i+k_h, j:j+k_w]
+            region_matrix[row_index] = region.reshape(-1)
+            row_index += 1
+            
+    output = t.matmul(region_matrix, kernel_matrix).reshape(out_h, out_w) + bias
+    return output
+
+
 x = t.randn(5,5)
 k = t.randn(3,3)
 b = t.randn(1)
 
 out = convolution(x, b, k)
+out = convolution_matrix(x, b, k)
 print(out)
+
+
+def convolution_batch(x, bias, kernel, stride=1, padding=0):
+    if padding > 0:
+        F.pad(x, (padding, padding, padding, padding, 0,0,0,0))
+
+    batch, in_c, in_h, in_w = x.shape
+    o_c, in_c, k_h, k_w = kernel.shape
+
+    out_h = (in_h - k_h) // stride + 1
+    out_w = (in_w - k_w) // stride + 1
+
+    output = t.zeros(batch, o_c, out_h, out_w, dtype=t.float32)
+
+    for bs in range(batch):
+        for oc in range(o_c):
+            for ic in range(in_c):
+                for i in range(0, in_h - k_h + 1, stride):
+                    for j in range(0, in_w - k_w + 1, stride):
+                        region = x[bs, ic, i:i+k_h, j:j+k_w]
+                        output[bs, oc, i//stride, j//stride] += t.sum(region * kernel[oc, ic])
+            output[bs, oc] += bias[oc]
+
+    return output
+
+x = t.randn(2, 3, 5, 5)
+k = t.randn(4, 3, 3, 3)
+bias = t.randn(4)
+y = convolution_batch(x, bias, k)
+print(y.shape)
